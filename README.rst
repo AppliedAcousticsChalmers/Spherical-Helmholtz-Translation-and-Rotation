44c1605c9508eb3cf4e2e6989d952117f2363071
Carl Andersson <carl.andersson@chalmers.se>
2020-04-08 15:52:25 +0200

    Creates development envorinment
    Makes sure that the package is installable, that versioning works,
    and that we define a developement environment.

---
+++ README.rst
@@ -0,0 +1,3 @@
+Fast-Helmholtz-Multipole-Methods
+================================
+Python implementations for fast multipole methods to solve the Helmholtz equation in three dimensions.
--- faheltzmm/__init__.py
+++ faheltzmm/__init__.py
@@ -1,1 +1,5 @@
 """FaHeltzMM, (Fast Hemlholtz Multipole Methods) is designed to solve the Helmholtz equation in three dimensions."""
+
+from . import _version
+__version__ = _version.__version__
+del _version  # Keeps the namespace clean!
