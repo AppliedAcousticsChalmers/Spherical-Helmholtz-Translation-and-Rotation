9e021fe0b367786e778b6542a316ed9d20bd743f
Carl Andersson <carl.andersson@chalmers.se>
2020-04-25 00:57:33 +0200

    Implements trigonometric coordinate system
    In certain cases it's useful to directly calculate the trigonometric
    function of the spherical angles, instead of the angles themselves.

--- faheltzmm/coordinates.py
+++ faheltzmm/coordinates.py
@@ -25,6 +25,43 @@ def cartesian_2_spherical(cartesian_positions):
     return np.stack([r, theta, phi], axis=0)
 
 
+def cartesian_2_trigonometric(cartesian_positions):
+    r"""Convert a cartesian position to the spherical variables.
+
+    This defines the spherical variable transform
+
+    .. math::
+        x = r \sin\theta\cos\phi
+        y = r \sin\theta\sin\phi
+        z = r \cos\theta
+
+    but instead of calculating the angles, values of the trigonometric functions
+    are returned. In many applications is is the trigonometric values that are of
+    interest, and it is more efficient to compute them directly.
+
+    Parameters
+    ----------
+    cartesian_positions: array_like
+        A 3x... shape array with the cartesian positions [x, y, z] in the
+        first axis.
+
+    Returns
+    -------
+    spherical_coordinates : np.ndarray
+        A 5x... shape array with the first axis corresponding to the radius,
+        :math:`\cos\theta`, :math:`\sin\theta`, :math:`\cos\phi`, and :math:`\sin\phi`
+    """
+    cartesian_positions = np.asarray(cartesian_positions)
+    r = np.sum(cartesian_positions**2, axis=0)**0.5
+    normalized = np.where(r == 0, 0, cartesian_positions / r)
+    cos_theta = np.clip(normalized[2], -1, 1)
+    sin_theta = (1 - cos_theta**2)**0.5
+    xy_projected = np.where(sin_theta == 0, 0, normalized[:2] / sin_theta)
+    cos_phi = np.clip(xy_projected[0], -1, 1)
+    sin_phi = np.clip(xy_projected[1], -1, 1)
+    return np.stack([r, cos_theta, sin_theta, cos_phi, sin_phi], axis=0)
+
+
 def spherical_2_cartesian(radius, colatitude, azimuth):
     """Convert spherical coordinates to cartesian positions.
 
--- tests/test_coordinates.py
+++ tests/test_coordinates.py
@@ -12,6 +12,14 @@ test_potisions = [
 
 
 @pytest.mark.parametrize("position", test_potisions)
-def test_invertability(position):
+def test_spherical_invertability(position):
     spherical_pos = faheltzmm.coordinates.cartesian_2_spherical(position)
     np.testing.assert_allclose(position, faheltzmm.coordinates.spherical_2_cartesian(*spherical_pos), rtol=1e-6, atol=1e-12)
+
+@pytest.mark.parametrize("position", test_potisions)
+def test_trigonometric_invertability(position):
+    r, cos_theta, sin_theta, cos_phi, sin_phi = faheltzmm.coordinates.cartesian_2_trigonometric(position)
+    x = r * sin_theta * cos_phi
+    y = r * sin_theta * sin_phi
+    z = r * cos_theta
+    np.testing.assert_allclose(position, np.stack([x, y, z], axis=0), rtol=1e-6, atol=1e-12)
