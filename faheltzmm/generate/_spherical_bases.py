07882c46f7e26aa2ba7a0387f6b1cdeabfac9d5a
Carl Andersson <carl.andersson@chalmers.se>
2020-05-25 09:45:27 +0200

    Allow indexing schemes for spherical bases
    This change will allow any of the implemented schemes
    in the generation of the spherical bases. It is noteworthy that the
    various sebset schemes are not particularly
    optimized at the moment.

--- faheltzmm/generate/_spherical_bases.py
+++ faheltzmm/generate/_spherical_bases.py
@@ -4,34 +4,36 @@ import numpy as np
 from ._spherical_harmonics import spherical_harmonics_all
 from ._bessel import spherical_jn_all, spherical_hn_all, spherical_yn_all
 from ..coordinates import cartesian_2_trigonometric
+from .. import indexing
 
 
-def spherical_base_all(max_order, position, wavenumber, domain, return_negative_m=True):
+def spherical_base_all(max_order, position, wavenumber, domain, indexing_scheme='natural'):
     r, cos_theta, _, cos_phi, sin_phi = cartesian_2_trigonometric(position)
     kr = r * np.reshape(wavenumber, np.shape(wavenumber) + (1,) * np.ndim(r))
 
+    radial_indices = indexing.expansions(max_order, 'natural', indexing_scheme)[0]
+
     if 'all' in domain.lower() or 'both' in domain.lower():
-        regular = spherical_jn_all(max_order=max_order, z=kr)[:, None]
-        singular = regular + 1j * spherical_yn_all(max_order=max_order, z=kr)[:, None]
+        regular = spherical_jn_all(max_order=max_order, z=kr)[radial_indices]
+        singular = regular + 1j * spherical_yn_all(max_order=max_order, z=kr)[radial_indices]
         radial = np.stack([regular, singular], axis=0)
     elif 'singular' in domain.lower() or 'exterior' in domain.lower():
-        radial = spherical_hn_all(max_order=max_order, z=kr)[:, None]
+        radial = spherical_hn_all(max_order=max_order, z=kr)[radial_indices]
     elif 'regular' in domain.lower() or 'interior' in domain.lower():
-        radial = spherical_jn_all(max_order=max_order, z=kr)[:, None]
+        radial = spherical_jn_all(max_order=max_order, z=kr)[radial_indices]
     else:
         raise ValueError(f'Unknown domain `{domain}`')
 
     angular = spherical_harmonics_all(
-        max_order=max_order, return_negative_m=True,
+        max_order=max_order, indexing_scheme=indexing_scheme,
         cosine_colatitude=cos_theta, azimuth=cos_phi + 1j * sin_phi,
     )
-    # TODO: This will give incorrect values if the compact output form is used!
-    return angular.reshape(angular.shape[:2] + (1,) * np.ndim(wavenumber) + angular.shape[2:]) * radial
+    return angular.reshape(angular.shape[:np.ndim(radial_indices)] + (1,) * np.ndim(wavenumber) + angular.shape[np.ndim(radial_indices):]) * radial
 
 
-def singular_base_all(max_order, position, wavenumber, return_negative_m=True):
-    return spherical_base_all(max_order=max_order, position=position, wavenumber=wavenumber, return_negative_m=return_negative_m, domain='singular')
+def singular_base_all(max_order, position, wavenumber, indexing_scheme='natural'):
+    return spherical_base_all(max_order=max_order, position=position, wavenumber=wavenumber, indexing_scheme=indexing_scheme, domain='singular')
 
 
-def regular_base_all(max_order, position, wavenumber, return_negative_m=True):
-    return spherical_base_all(max_order=max_order, position=position, wavenumber=wavenumber, return_negative_m=return_negative_m, domain='regular')
+def regular_base_all(max_order, position, wavenumber, indexing_scheme='fulnaturall'):
+    return spherical_base_all(max_order=max_order, position=position, wavenumber=wavenumber, indexing_scheme=indexing_scheme, domain='regular')
--- tests/test_spherical_bases.py
+++ tests/test_spherical_bases.py
@@ -6,6 +6,7 @@ import faheltzmm.generate._spherical_bases
 import faheltzmm.generate._spherical_harmonics
 import faheltzmm.generate._bessel
 import faheltzmm.coordinates
+import faheltzmm.indexing
 
 np.random.seed(0)
 max_order = 12
@@ -34,3 +35,37 @@ def test_bases_generation(base):
             manual[order, -mode] = radial_func(order, kr) * scipy.special.sph_harm(-mode, order, phi, theta)
 
     np.testing.assert_allclose(manual, all_orders)
+
+
+@pytest.mark.parametrize('base', ['regular', 'singular'])
+@pytest.mark.parametrize('indexing_scheme', [
+    'natural', 'compact', 'linear', 'natural non-negative', 'compact non-negative',
+    'zonal', 'positive', 'negative', 'non-positive', 'non-negative',
+    'sectorial', 'sectorial positive', 'sectorial negative', 'sectorial non-positive', 'sectorial non-negative',
+    'tesseral', 'positive tesseral', 'negative tesseral',
+])
+def test_indexing_schemes(base, indexing_scheme):
+    natural = faheltzmm.generate.spherical_base_all(max_order, positions, wavenumbers, base, 'natural')
+    scheme = faheltzmm.generate.spherical_base_all(max_order, positions, wavenumbers, base, indexing_scheme)
+    natural_2_scheme = faheltzmm.indexing.expansions(natural, 'natural', indexing_scheme)
+
+    np.testing.assert_allclose(scheme, natural_2_scheme)
+
+
+@pytest.mark.parametrize('order', [0, 1, 3, 7])
+@pytest.mark.parametrize('indexing_scheme', ['natural', 'compact', 'linear'])
+@pytest.mark.parametrize('position, wavenumber', [
+    (np.random.normal(size=3), np.random.uniform()),
+    (np.random.normal(size=(3, 5)), np.random.uniform()),
+    (np.random.normal(size=(3, 5, 7)), np.random.uniform()),
+    (np.random.normal(size=3), np.random.uniform(size=(5))),
+    (np.random.normal(size=3), np.random.uniform(size=(5, 7))),
+    (np.random.normal(size=(3, 5)), np.random.uniform(size=(11))),
+    (np.random.normal(size=(3, 2, 3)), np.random.uniform(size=(4, 5))),
+])
+def test_dual_domain(order, position, wavenumber, indexing_scheme):
+    regular_singular = faheltzmm.generate.spherical_base_all(order, position, wavenumber, 'both', indexing_scheme)
+    regular = faheltzmm.generate.spherical_base_all(order, position, wavenumber, 'regular', indexing_scheme)
+    singular = faheltzmm.generate.spherical_base_all(order, position, wavenumber, 'singular', indexing_scheme)
+    np.testing.assert_allclose(regular, regular_singular[0])
+    np.testing.assert_allclose(singular, regular_singular[1])
