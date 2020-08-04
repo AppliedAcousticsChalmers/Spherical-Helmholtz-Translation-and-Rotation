eab83dba52d7e64be75f8a1d9fabf546845dc87f
CarlAndersson <carl.andersson@chalmers.se>
2020-08-04 15:16:59 +0200

    Implements base class for expansions
    All expansions will share a similar structure with regards to indexing.
    They will also share numerical properties, e.g. multiplication or
    addition.
    
    The idea is to subclass this for specific expansion types, e.g.
    incident field or sources, which can then use appropriate methods
    to translate or rotate etc.

---
+++ faheltzmm/expansions.py
@@ -0,0 +1,90 @@
+import numpy as np
+
+
+class Expansion:
+    def __init__(self, order=None, shape=None, data=None, wavenumber=None):
+        self._wavenumber = wavenumber
+
+        if data is None:
+            self._order = order
+            self._shape = () if shape is None else shape
+            num_unique = (self.order + 1) ** 2
+            self._data = np.zeros((num_unique,) + np.shape(self.wavenumber) + self.shape, dtype=complex)
+        else:
+            self._order = (len(data) ** 0.5 - 1) // 1
+            self._shape = np.shape(data)[1 + np.ndim(self.wavenumber):]
+            self._data = data
+
+    @property
+    def order(self):
+        return self._order
+
+    @property
+    def shape(self):
+        return self._shape
+
+    @property
+    def wavenumber(self):
+        return self._wavenumber
+
+    def _idx(self, order, mode):
+        if order > self.order:
+            raise IndexError(f'Order {order} out of bounds for {self.__class__.__name__} with max order {self.order}')
+        if abs(mode) > order:
+            raise IndexError(f'Mode {mode} out of bounds for order {order}')
+        return order ** 2 + mode
+
+    def __getitem__(self, key):
+        n, m, = key
+        return self._data[self._idx(n, m)]
+
+    def __setitem__(self, key, value):
+        n, m, = key
+        self._data[self._idx(n, m)] = value
+
+    def _compatible_with(self, other):
+        # TODO: Raise meaningful exceptions when the objects are not compatible.
+        return (type(self) == type(other)
+                and self.order == other.order
+                and self.shape == other.shape
+                and (self.wavenumber is other.wavenumber or np.allclose(self.wavenumber, other.wavenumber))
+                )
+
+    def __add__(self, other):
+        if self._compatible_with(other):
+            return type(self)(data=self._data + other._data, wavenumber=self.wavenumber)
+        return NotImplemented
+
+    def __iadd__(self, other):
+        if self._compatible_with(other):
+            self._data += other._data
+            return self
+        return NotImplemented
+
+    def __sub__(self, other):
+        if self._compatible_with(other):
+            return type(self)(data=self._data - other._data, wavenumber=self.wavenumber)
+        return NotImplemented
+
+    def __isub__(self, other):
+        if self._compatible_with(other):
+            self._data -= other._data
+            return self
+        return NotImplemented
+
+    def __mul__(self, other):
+        return type(self)(data=self._data * other, wavenumber=self.wavenumber)
+
+    def __imul__(self, other):
+        self._data *= other
+        return self
+
+    def __div__(self, other):
+        return type(self)(data=self._data / other, wavenumber=self.wavenumber)
+
+    def __idiv__(self, other):
+        self._data /= other
+        return self
+
+    def __neg__(self):
+        return type(self)(data=-self._data, wavenumber=self.wavenumber)
