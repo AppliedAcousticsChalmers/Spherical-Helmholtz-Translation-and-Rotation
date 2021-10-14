0ef1431d7edd82e45130453ffafdda7641f8891b
Carl Andersson <carl.andersson@chalmers.se>
2021-10-14 17:48:14 +0200

    Merge the pure cython functions into the python functions
    This seems to be equally fast as keeping the loops in separate functions,
    so there is no real need to define a separate function just for the
    main loop.

--- shetar/bases/_legendre_cython.pyx
+++ shetar/bases/_legendre_cython.pyx
@@ -4,9 +4,8 @@ cimport cython
 
 def legendre_polynomials(x, order=None, out=None):
     if order is None and out is None:
-        raise ValueError('Cannot calculate Legendre polynomians without receiving either the output arrey or the order')
+        raise ValueError('Cannot calculate Legendre polynomials without receiving either the output array or the order')
     output_shape = np.shape(x)
-    num_elements = np.size(x)
 
     if order is None:
         order = out.shape[0] - 1
@@ -14,34 +13,29 @@ def legendre_polynomials(x, order=None, out=None):
         out = np.zeros((order + 1,) + output_shape)
 
     out[0] = 1 / 2**0.5
-    if order > 1:
+    if order >= 1:
         out[1] = x * 1.5**0.5
 
-    if order >= 2:
-        legendre_recurrence(x.reshape(-1), out.reshape((order + 1, num_elements)), order, num_elements)
+    cdef:
+        int idx, n, N = order, num_elements = np.size(x)
+        double n_minus_1_factor, n_minus_2_factor
+        double[:] x_cy = x.reshape(-1)
+        double[:, :] data = out.reshape((order + 1, np.size(x)))
+
+    with cython.boundscheck(False), cython.cdivision(True), nogil:
+        for n in range(2, N + 1):
+            n_minus_1_factor = ((2 * n + 1) * (2 * n - 1))**0.5 / n
+            n_minus_2_factor = <double>(n - 1) / n * (<double>(2 * n + 1) / (2 * n - 3))**0.5
+            for idx in range(num_elements):
+                data[n, idx] = x_cy[idx] * data[n - 1, idx] * n_minus_1_factor - data[n - 2, idx] * n_minus_2_factor
 
     return out
 
 
-@cython.boundscheck(False)
-@cython.cdivision(True)
-cdef void legendre_recurrence(double[:] x, double[:, :] data, int order, int num_elements) nogil:
-    cdef:
-        int idx, n
-        double n_minus_1_factor, n_minus_2_factor,
-
-    for n in range(2, order + 1):
-        n_minus_1_factor = ((2 * n + 1) * (2 * n - 1))**0.5 / n
-        n_minus_2_factor = <double>(n - 1) / n * (<double>(2 * n + 1) / (2 * n - 3))**0.5
-        for idx in range(num_elements):
-            data[n, idx] = x[idx] * data[n - 1, idx] * n_minus_1_factor - data[n - 2, idx] * n_minus_2_factor
-
-
 def associated_legendre_polynomials(x, order=None, out=None):
     if order is None and out is None:
-        raise ValueError('Cannot calculate Legendre polynomians without receiving either the output arrey or the order')
+        raise ValueError('Cannot calculate associated Legendre polynomials without receiving either the output array or the order')
     output_shape = np.shape(x)
-    num_elements = np.size(x)
 
     if order is None:
         num_unique = out.shape[0]
@@ -52,25 +46,16 @@ def associated_legendre_polynomials(x, order=None, out=None):
 
     out[0] = 1 / 2**0.5
 
-    if order >= 1:
-        one_minus_x_square = 1 - x**2
-        associated_legendre_recurrence(
-            x.reshape(num_elements),
-            one_minus_x_square.reshape(num_elements),
-            out.reshape((num_unique, num_elements)),
-            order, num_elements
-        )
-
-    return out
-
-@cython.boundscheck(False)
-@cython.cdivision(True)
-cdef void associated_legendre_recurrence(double[:] x, double[:] one_minus_x_square, double[:, :] data, int order, int num_elements) nogil:
     cdef:
         int idx_elem, n, m, idx_assign, idx_m1, idx_m2
+        int N = order, num_elements = np.size(x)
         double fact_m1, fact_m2
+        double[:] x_cy = x.reshape(-1)
+        double[:] one_minus_x_square = 1 - x.reshape(-1)**2
+        double[:, :] data = out.reshape((num_unique, -1))
 
-    for n in range(1, order + 1):
+    with cython.boundscheck(False), cython.cdivision(True), nogil:
+        for n in range(1, N + 1):
             # Recurrence to higher orders
             idx_assign = associated_legendre_index(n, n)
             idx_m1 = associated_legendre_index(n - 1, n - 1)
@@ -83,7 +68,7 @@ cdef void associated_legendre_recurrence(double[:] x, double[:] one_minus_x_squa
             idx_m1 = associated_legendre_index(n, n)
             fact_m1 = - (2 * n)**0.5
             for idx_elem in range(num_elements):
-                data[idx_assign, idx_elem] = fact_m1 * data[idx_m1, idx_elem] * x[idx_elem]
+                data[idx_assign, idx_elem] = fact_m1 * data[idx_m1, idx_elem] * x_cy[idx_elem]
 
             for m in range(n - 2, -1, -1):  # [n - 2, n - 3, ... 0]
                 # Recurrece to lower modes
@@ -91,12 +76,15 @@ cdef void associated_legendre_recurrence(double[:] x, double[:] one_minus_x_squa
                 idx_m1 = associated_legendre_index(n, m + 1)
                 idx_m2 = associated_legendre_index(n, m + 2)
                 fact_m1 = - 2 * <double>(m + 1) / ((n + m + 1) * (n - m)) ** 0.5
-                fact_m2 = -( <double>((n + m + 2) * (n - m - 1)) / ((n - m) * (n + m + 1))) ** 0.5
+                fact_m2 = -(<double>((n + m + 2) * (n - m - 1)) / ((n - m) * (n + m + 1))) ** 0.5
                 for idx_elem in range(num_elements):
                     data[idx_assign, idx_elem] = (
-                        fact_m1 * data[idx_m1, idx_elem] * x[idx_elem]
+                        fact_m1 * data[idx_m1, idx_elem] * x_cy[idx_elem]
                         + fact_m2 * data[idx_m2, idx_elem] * one_minus_x_square[idx_elem]
-                    )
+                    ) 
+
+    return out
+
 
 
 @cython.boundscheck(False)
